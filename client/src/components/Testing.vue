8f2c1b04e5694b0cbeb6f68f6b5cc777373cbdc4
Suvi Vappula <suvi.vappula@myy.haaga-helia.fi>
2019-05-03 19:15:36 +0300

    years and emissions to table

--- client/src/components/Testing.vue
+++ client/src/components/Testing.vue
@@ -1,18 +1,34 @@
 <template>
   <div>
-    <h1>CO2-emissions</h1>
-    <!-- <p>{{ countries }}</p> -->
-    <!-- <div v-for="country in this.countries" :key="country.id">
-      <p>{{ country }}</p>
-    </div> -->
-    <select v-model="selected">
-      <option v-for="country in this.countries" :key="country.id" v-on:click="getData()">
-        {{ country }}
-      </option>
-    </select>
-    <p>{{ this.selected }}</p>
-    <p>{{ this.emissions }}</p>
-    <p>{{ this.years }}</p>
+    <div>
+      <h1>CO2-emissions</h1>
+      <!-- <p>{{ countries }}</p> -->
+      <!-- <div v-for="country in this.countries" :key="country.id">
+        <p>{{ country }}</p>
+      </div> -->
+      <select v-model="selected">
+        <option v-for="country in this.countries" :key="country.id" v-on:click="getData()">
+          {{ country }}
+        </option>
+      </select>
+      <div class="checkbox">
+        <label><input type="checkbox" v-model="per_capita" >Per capita</label>
+      </div>
+      <p>{{ this.selected }}</p>
+    </div>
+    <div class="emissionstable">
+      <table>
+        <tr>
+          <th>Years</th>
+          <th>Emissions</th>
+        </tr>
+        <tr v-for="(emission, index) in this.emissions">
+          <td> {{ years[index] }} </td>
+          <td> {{emission}} </td>
+        </tr>
+      </table>
+      <!-- <b-table hover :fields="fields" :items="years, emissions"></b-table> -->
+    </div>
   </div>
 </template>
 
@@ -23,10 +39,11 @@ export default {
   name: 'Testing',
   data() {
     return {
+      fields: ['years','emissions'],
       countries: [],
       selected: '',
       emissions: [],
-      years: []
+      years: [],
       //msg: 'Welcome to Your Vue.js App',
     };
   },
@@ -61,6 +78,16 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style lang="css">
+  table, th, tr{
+    width:50%;
+    margin-left:25%;
+    margin-right:15%;
+    border: 1px solid #ddd;
+    padding: 5px;
+    border-collapse: collapse;
+  }
+  .checkbox{
+    padding-top: 10px;
+  }
 </style>
